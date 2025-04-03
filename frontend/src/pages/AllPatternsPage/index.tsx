474c6330565d549a2644d64e2897e71de3eec498
Nadezhda Melnikova <naryvkinan@mail.ru>
2025-04-03 17:54:42 +0300

    adds englishName to pattern

--- frontend/src/pages/AllPatternsPage/index.tsx
+++ frontend/src/pages/AllPatternsPage/index.tsx
@@ -17,7 +17,10 @@ export const AllPatternsPage = () => {
       <h1>All Patterns</h1>
       {data?.patterns.map((pattern) => (
         <div key={pattern.id}>
-          <h2>{pattern.name}</h2>
+          <h2>
+            <span>{pattern.name}</span>
+            <span> ({pattern.englishName})</span>
+          </h2>
           <p>{pattern.description}</p>
         </div>
       ))}
