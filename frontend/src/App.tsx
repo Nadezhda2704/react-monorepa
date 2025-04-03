cf584fa4dd7f8f4c1593d59008cd5c064c339fcb
Nadezhda Melnikova <naryvkinan@mail.ru>
2025-04-03 14:09:18 +0300

    adds trpc to front

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { TrpcProvider } from './lib/trpc';
+import { AllPatternsPage } from './pages/AllPatternsPage';
+
+const App = () => {
+  return (
+    <TrpcProvider>
+      <AllPatternsPage />
+    </TrpcProvider>
+  );
+};
+
+export default App;
---
+++ frontend/src/pages/AllPatternsPage/index.tsx
@@ -0,0 +1,26 @@
+import { trpc } from '../../lib/trpc';
+
+export const AllPatternsPage = () => {
+  // console.log('trpc.getPatterns.useQuery()', trpc.getPatterns.useQuery());
+  const { data, error, isLoading, isFetching, isError } = trpc.getPatterns.useQuery();
+
+  if (isLoading || isFetching) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
+
+  return (
+    <div>
+      <h1>All Patterns</h1>
+      {data?.patterns.map((pattern) => (
+        <div key={pattern.id}>
+          <h2>{pattern.name}</h2>
+          <p>{pattern.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
