478b90c0321521a2b3ff0612d4ee2543c2f6f530
Nadezhda Melnikova <naryvkinan@mail.ru>
2025-04-14 23:47:35 +0300

    add prisma

---
+++ backend/src/prisma/migrations/20250414202828_add_pattern_model/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Pattern" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "englishName" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+
+    CONSTRAINT "Pattern_pkey" PRIMARY KEY ("id")
+);
