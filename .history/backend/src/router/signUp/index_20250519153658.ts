f09cf1bd6e01a47365afda0b66dbb0ce6fc450e8
Nadezhda Melnikova <naryvkinan@mail.ru>
2025-05-19 15:43:05 +0300

    authorization: login

--- .history/backend/src/router/signUp/index_20250519153658.ts
+++ .history/backend/src/router/signUp/index_20250519153658.ts
@@ -1,6 +1,7 @@
 import crypto from 'crypto';
 import { trpc } from '../../lib/trpc';
 import { zSignUpTrpcInput } from './input';
+import { getPasswordHash } from '../../utils/getPasswordHash';
 
 export const signUpTrpcRoute = trpc.procedure.input(zSignUpTrpcInput).mutation(async ({ ctx, input }) => {
   const exUser = await ctx.prisma.user.findUnique({
@@ -9,12 +10,12 @@ export const signUpTrpcRoute = trpc.procedure.input(zSignUpTrpcInput).mutation(a
     },
   });
   if (exUser) {
-    throw new Error('User with this nick already exists');
+    throw new Error('Такой ник уже существует ✍🏻');
   }
   await ctx.prisma.user.create({
     data: {
       nick: input.nick,
-      password: crypto.createHash('sha256').update(input.password).digest('hex'),
+      password: getPasswordHash(input.password),
     },
   });
   return true;
