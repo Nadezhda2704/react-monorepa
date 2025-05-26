b3f614a0f985bdcc17fd29e8f1a255af7e0d776c
Nadezhda Melnikova <naryvkinan@mail.ru>
2025-05-26 22:23:02 +0300

    refactor get user from jwt token

--- backend/src/router/postReadPattern/index.ts
+++ backend/src/router/postReadPattern/index.ts
@@ -1,35 +1,44 @@
 import { trpc } from '../../lib/trpc';
 import { zPostReadPatternTrpcInput } from './input';
 
-export const postReadPatternTrpcRoute = trpc.procedure.input(zPostReadPatternTrpcInput).mutation(async ({ ctx, input }) => {
-  const userId = input.userId;
-  const user = await ctx.prisma.user.findFirst({
-    where: {
-      id: userId,
-    },
-  });
-  if (!user) {
-    throw new Error('Пользователь не найден');
-  }
+export const postReadPatternTrpcRoute = trpc.procedure
+  .input(zPostReadPatternTrpcInput)
+  .mutation(async ({ ctx, input }) => {
+    const authorizedUserId = ctx.authorizedUser?.id;
+    let user = null;
 
-  let updatedReadPatterns: string[] = [];
-  const addingPatternId = input.patternId;
-
-  if (!user.readPatterns.length) {
-    updatedReadPatterns = [addingPatternId];
-  } else {
-    const currentTags = user.readPatterns;
-    if (!currentTags.includes(addingPatternId)) {
-      updatedReadPatterns = [...currentTags, addingPatternId];
+    if (authorizedUserId) {
+      user = await ctx.prisma.user.findFirst({
+        where: {
+          id: authorizedUserId,
+        },
+      });
     } else {
-      console.log('Id уже находится в массиве');
+      throw new Error('Пользователь не авторизован');
     }
-  }
 
-  await ctx.prisma.user.update({
-    where: { id: userId },
-    data: { readPatterns: updatedReadPatterns },
+    if (!user) {
+      throw new Error('Пользователь не найден');
+    }
+
+    let updatedReadPatterns: string[] = [];
+    const addingPatternId = input.patternId;
+
+    if (!user.readPatterns.length) {
+      updatedReadPatterns = [addingPatternId];
+    } else {
+      const currentTags = user.readPatterns;
+      if (!currentTags.includes(addingPatternId)) {
+        updatedReadPatterns = [...currentTags, addingPatternId];
+      } else {
+        console.log('Id уже находится в массиве');
+      }
+    }
+
+    await ctx.prisma.user.update({
+      where: { id: authorizedUserId },
+      data: { readPatterns: updatedReadPatterns },
+    });
+
+    return { readPatterns: updatedReadPatterns };
   });
-
-  return { readPatterns: updatedReadPatterns };
-});
--- backend/src/router/postReadPattern/input.ts
+++ backend/src/router/postReadPattern/input.ts
@@ -2,5 +2,4 @@ import { z } from 'zod';
 
 export const zPostReadPatternTrpcInput = z.object({
   patternId: z.string().min(1),
-  userId: z.string().min(1),
 });
