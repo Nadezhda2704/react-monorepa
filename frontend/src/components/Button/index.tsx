602e298506c25997ee2d8561a644947498eb8e39
Nadezhda Melnikova <naryvkinan@mail.ru>
2025-05-29 13:11:29 +0300

    mark read

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -1,9 +1,24 @@
 import cn from 'classnames';
 import css from './index.module.scss';
 
-export const Button = ({ children, loading = false }: { children: React.ReactNode; loading?: boolean }) => {
+export const Button = ({
+  children,
+  loading = false,
+  type = 'submit',
+  click = () => {},
+}: {
+  children: React.ReactNode;
+  loading?: boolean;
+  type?: 'submit' | 'button' | 'reset' | undefined;
+  click?: Function;
+}) => {
   return (
-    <button className={cn({ [css.button]: true, [css.disabled]: loading })} type="submit" disabled={loading}>
+    <button
+      className={cn({ [css.button]: true, [css.disabled]: loading })}
+      type={type}
+      disabled={loading}
+      onClick={click}
+    >
       {loading ? 'Отправка...' : children}
     </button>
   );
--- frontend/src/pages/PatternPage/index.tsx
+++ frontend/src/pages/PatternPage/index.tsx
@@ -3,13 +3,40 @@ import { GetPatternRouteParams } from '../../lib/routes';
 import { trpc } from '../../lib/trpc';
 import css from './index.module.scss';
 import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
+import { Informer } from '../../components/Informer';
+import React from 'react';
 
 export const PatternPage = () => {
+  const [upReadPatterns, setUpReadPatterns] = React.useState([]);
+
   const { patternId } = useParams() as GetPatternRouteParams;
 
   const { data, error, isLoading, isFetching, isError } = trpc.getPattern.useQuery({
     patternId,
   });
+
+  let getReadPatterns: string[] = [];
+  if (!getReadPatterns.length) {
+    // toDO обработка ошибок статус загрузки
+    getReadPatterns = trpc.getReadPatterns.useQuery().data?.readPatterns;
+  }
+
+  const markStudiedMutation = trpc.postReadPattern.useMutation();
+
+  const markStudied = () => {
+    markStudiedMutation.mutate(
+      { patternId },
+      {
+        onSuccess: ({ readPatterns }) => {
+          getReadPatterns = readPatterns;
+          setUpReadPatterns(readPatterns);
+        },
+      }
+    );
+  };
+
+  // ToDo мутацию для кнопки к прочтению, использовать существующий стейт
 
   if (isLoading || isFetching) {
     return <span>Loading...</span>;
@@ -29,10 +56,21 @@ export const PatternPage = () => {
         <>
           <span>{data.name}</span>
           <span> ({data.englishName})</span>
+          {(upReadPatterns.length
+            ? upReadPatterns.includes(patternId)
+            : getReadPatterns && getReadPatterns.includes(patternId)) && <span> *Изучен*</span>}
         </>
       }
       description={data.description}
     >
+      <div className={css.wrapBtn}>
+        <Button type="button" click={markStudied} loading={markStudiedMutation.isPending}>
+          Изучен
+        </Button>
+        {markStudiedMutation.error && <Informer color="red">Что-то пошло не так</Informer>}
+        <Button type="button">Повторить</Button>
+      </div>
+
       <div className={css.wrap}>
         <a className={css.item} href="#">
           read book
